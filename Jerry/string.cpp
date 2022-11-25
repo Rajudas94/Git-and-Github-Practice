b18952dc6f52973a559354d5041a54e5e68998ef
Raju Das <rajud878@gmail.com>
2022-11-25 22:51:08 +0530

    Added 3 loops statements

--- Jerry/string.cpp
+++ Jerry/string.cpp
@@ -9,5 +9,16 @@ int main()
    int a = 5, b = 6;
    cout << a+b << "\n";
 
+   //demonstration 3 kinds of loops
+   //for loop
+   for(int i=0; i<30; i+=10) cout << i << " " ;
+
+   //while loop
+   int x = 10;
+   while(x--) { cout << x << " " ;}
+
+   //advance for loop or as the interviewers like to put it "advance for loop"
+   for(auto x : "data structure") { cout << x << " " ; }
+
    return 0;
 }
